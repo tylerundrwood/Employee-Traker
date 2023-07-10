d8a2da87bd67329f11bdea89e4c9c623ae19c9f9
Tyler Underwood <twood9600@yahoo.com>
2023-07-09 20:24:37 -0400

    created the table roles

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,24 @@ CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
