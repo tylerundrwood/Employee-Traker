a54622d0049ca71b35348b3d40d2f0a058a767b6
Tyler Underwood <twood9600@yahoo.com>
2023-07-10 18:24:13 -0400

    created starter questions

--- index.js
+++ index.js
@@ -17,3 +17,53 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the Company_DB.`)
   );
+
+  const startApp = () => {
+    return inquirer.prompt
+        ([
+           {
+            type: "list",
+            name: "first",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Update Employee Role","Add Employee", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"]
+           }
+        ])
+        .then(function(data) {
+            if(data.first == "View All Employees") {
+                // Query employees table
+                db.query('SELECT * FROM employee', function (err, results) {
+                    console.table(results);
+                    return startApp();
+                });
+            } else if(data.first == "View All Departments") {
+                // Query department table
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.table(results);
+                    return startApp();
+                });
+            } else if(data.first == "View All Roles") {
+                // Query role table
+                db.query('SELECT * FROM role', function (err, results) {
+                    console.table(results);
+                    return startApp();
+                });
+            } else if(data.first == "Add Department") {
+                // run function to add department
+                addDepartmentQuestions();
+            } else if(data.first == "Add Employee") {
+                // run function to add employee
+                addEmployeeQuestions();
+            } else if(data.first == "Add Role") {
+                // run function to add roll
+                addRoleQuestions();
+            } else if(data.first == "Update Employee Role") {
+                //updateEmployeeRole();
+                console.log("Currently Unavailable.")
+                return startApp()
+            }
+             else if(data.first == "Quit") {
+                // if user selects 'quit' the app exits
+                process.exit(console.log("Goodbye!"));
+            }
+        })
+}
