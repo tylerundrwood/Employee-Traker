1c16f9921e778b0856f2631e241582ab2f1ac01e
Tyler Underwood <twood9600@yahoo.com>
2023-07-12 17:23:20 -0400

    added department and employee questions

--- index.js
+++ index.js
@@ -67,3 +67,102 @@ const db = mysql.createConnection(
             }
         })
 }
+const addDepartmentQuestions = () => {
+    return inquirer.prompt
+        ([
+            {
+                type: "input",
+                name: "departmentName",
+                message: "What is the name of the new department?",
+                validate: DepartmentNameInput => {
+                    if(DepartmentNameInput) {
+                        return true
+                    } else {
+                        console.log("Please input a department name.")
+                    }
+                }
+            }
+        ])
+        .then(function(department) {
+            const bestDepartment = new NewDepartment (department.departmentName)
+            // deconstruct newDepartment object to easily insert into query
+            const name = bestDepartment.name
+            // query new department name into table
+            db.query('INSERT INTO department (name) VALUES (?)', name, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return startApp();
+                } else {
+                    console.log('New Department Added!');
+                    return startApp();
+                }
+            })
+        })
+}
+
+// add role questions
+const addRoleQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "roleTitle",
+            message: "What is the name of the new role?",
+            validate: roleNameInput => {
+                if(roleNameInput) {
+                    return true
+                } else {
+                    console.log("Please input a name for this new role.")
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "roleSalary",
+            message: "What is the salary for this position?",
+            validate: roleSalaryInput => {
+                if(roleSalaryInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a salary for this new role.")
+                }
+            }
+        },
+    ]).then(function(roleInput) {
+        
+        let title = roleInput.roleTitle
+        let salary = roleInput.roleSalary
+        db.promise().query("SELECT id AS name FROM department")
+            .then(([row, fields]) => {
+                return inquirer.prompt ({
+                    type: "list",
+                    name: "roleDepartment",
+                    message: "What department will this role be in?",
+                    choices: row.filter(r => !!r.name).map(r => r.name), 
+                    validate: roleDepartmentInput => {
+                        if(roleDepartmentInput) {
+                            return true;
+                        } else {
+                            console.log("Please input what department this new role belongs to.")
+                        }
+                    }
+                })
+            }).then(function(roleDep) {
+                let department_id = roleDep.roleDepartment
+                console.log(title)
+                console.log(salary)
+                console.log(department_id)
+                let values = [
+                    [title, salary, department_id]
+                ]
+                    db.query("INSERT INTO role (title, salary, department_id) VALUES (?)", values, (err, res) => {
+                        if(err) {
+                            console.log(err)
+                            return startApp()
+                        } else {
+                            console.log("New Role Created!")
+                            return startApp()
+                        }
+                    })
+              })
+    })     
+}
